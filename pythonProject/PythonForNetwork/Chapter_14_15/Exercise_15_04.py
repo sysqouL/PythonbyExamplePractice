ecf83eef99737f4e73811af9c2fa9e23a63d3db0
sysqouL <ziminnik94@gmail.com>
2022-02-02 16:40:39 +0300

    Python для сетевых инженеров - глава 15 работа с регулярными выражениями. Решение 15.4

---
+++ pythonProject/PythonForNetwork/Chapter_14_15/Exercise_15_04.py
@@ -0,0 +1,44 @@
+#-------------------------------------------------
+"""
+"""
+
+"""
+    Задание 15.4
+    Создать функцию get_ints_without_description, которая ожидает как аргумент
+    имя файла, в котором находится конфигурация устройства.
+    Функция должна обрабатывать конфигурацию и возвращать список имен интерфейсов,
+    на которых нет описания (команды description).
+    Пример интерфейса с описанием:
+    interface Ethernet0/2
+     description To P_r9 Ethernet0/2
+     ip address 10.0.19.1 255.255.255.0
+     mpls traffic-eng tunnels
+     ip rsvp bandwidth
+    Интерфейс без описания:
+    interface Loopback0
+     ip address 10.1.1.1 255.255.255.255
+    Проверить работу функции на примере файла config_r1.txt.
+"""
+
+import re
+from pprint import pprint
+
+def get_ints_without_description(config):
+    """
+    - в if - если descr группа None
+    :param config: файл конфигурации
+    :return: result - список интерфейсов на которых нету description
+    """
+    result = []
+    regex = (r"!\ninterface (?P<intf>\S+)\n"
+             r"(?P<descr> description \S+)?")
+    with open(config) as f:
+        for match in re.finditer(regex, f.read()):
+            # print(match.groups())
+            if match.lastgroup == 'intf':
+                result.append(match.group('intf'))
+
+    return result
+
+if __name__ == "__main__":
+    pprint(get_ints_without_description("config_r1.txt"))

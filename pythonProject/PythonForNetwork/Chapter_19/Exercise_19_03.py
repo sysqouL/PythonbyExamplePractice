5ea0f713c4d5be05c536835fc53173034e5bddf9
sysqouL <ziminnik94@gmail.com>
2022-03-05 05:44:35 +0300

    Python для сетевых инженеров - глава 19 Подключение к нескольким устройствам. Решение 19.3

---
+++ pythonProject/PythonForNetwork/Chapter_19/Exercise_19_03.py
@@ -0,0 +1,55 @@
+#-------------------------------------------------
+"""
+"""
+
+"""
+    Создать функцию send_command_to_devices, которая отправляет разные
+    команды show на разные устройства в параллельных потоках, а затем записывает
+    вывод команд в файл. Вывод с устройств в файле может быть в любом порядке.
+    Параметры функции:
+    * devices - список словарей с параметрами подключения к устройствам
+    * commands_dict - словарь в котором указано на какое устройство отправлять
+      какую команду. Пример словаря - commands
+    * filename - имя файла, в который будут записаны выводы всех команд
+    * limit - максимальное количество параллельных потоков (по умолчанию 3)
+    Функция ничего не возвращает.
+    Вывод команд должен быть записан в файл
+"""
+
+from itertools import repeat
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from netmiko import ConnectHandler, NetMikoTimeoutException
+import yaml
+
+
+commands = {
+    "192.168.75.133": "disp ip int br",
+    "192.168.75.134": "disp int desc",
+    "192.168.75.135": "disp ver",
+}
+
+
+def send_show_command(device, command):
+    with ConnectHandler(**device) as ssh:
+        ssh.enable()
+        result = ssh.send_command(command)
+        prompt = ssh.find_prompt()
+    return f"{prompt}{command}\n{result}\n"
+
+
+def send_command_to_devices(devices, commands_dict, filename, limit=3):
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        futures = [
+            executor.submit(send_show_command, device, commands_dict[device["host"]])
+            for device in devices
+        ]
+        with open(filename, "w") as f:
+            for future in as_completed(futures):
+                f.write(future.result())
+
+
+if __name__ == "__main__":
+    with open("devices_huawei.yaml") as f:
+        devices = yaml.safe_load(f)
+    send_command_to_devices(devices, commands, "result2.txt")

fd36ad4c65e60e32937c8a31b51639e6471f9110
sysqouL <ziminnik94@gmail.com>
2022-04-24 07:07:28 +0300

    Python для сетевых инженеров - глава 21 TextFSM. Решение 21.05

---
+++ pythonProject/PythonForNetwork/Chapter_21_TextFSM/Exercise_21_05.py
@@ -0,0 +1,44 @@
+#-------------------------------------------------
+"""
+"""
+
+"""
+    Задание 21.5
+    Создать функцию send_and_parse_command_parallel.
+    Функция send_and_parse_command_parallel должна запускать в
+    параллельных потоках функцию send_and_parse_show_command из задания 21.4.
+    Параметры функции send_and_parse_command_parallel:
+    * devices - список словарей с параметрами подключения к устройствам
+    * command - команда
+    * templates_path - путь к каталогу с шаблонами TextFSM
+    * limit - максимальное количество параллельных потоков (по умолчанию 3)
+    Функция должна возвращать словарь:
+    * ключи - IP-адрес устройства с которого получен вывод
+    * значения - список словарей (вывод который возвращает функция send_and_parse_show_command)
+    Проверить работу функции на примере вывода команды sh ip int br
+    и устройствах из devices.yaml.
+"""
+
+from Exercise_21_04 import send_and_parse_show_command
+from concurrent.futures import ThreadPoolExecutor
+from pprint import pprint
+import os
+import yaml
+
+
+def send_and_parse_command_parallel(devices, command, templates_path, limit=3):
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        result_all = [
+            executor.submit(send_and_parse_show_command, device, command, templates_path)
+            for device in devices
+        ]
+        output = {device["host"]: f.result() for device, f in zip(devices, result_all)}
+    return output
+
+
+if __name__ == "__main__":
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+    command = "sh ip int br"
+    path_dir = f"{os.getcwd()}/templates"
+    pprint(send_and_parse_command_parallel(devices, command, path_dir))

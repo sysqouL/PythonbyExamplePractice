2621af630664beb21c0653f4a3488393dff7ffa8
sysqouL <ziminnik94@gmail.com>
2022-05-06 05:09:45 +0300

    Python для сетевых инженеров - глава 22 ООП. Решение 22.1a проверка дублирующих линков вынесена в метод _normalize

---
+++ pythonProject/PythonForNetwork/Chapter_22_OOP/Exercise_22_1a.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------
+"""
+"""
+
+"""
+    Задание 22.1a
+    Скопировать класс Topology из задания 22.1 и изменить его.
+    Перенести функциональность удаления "дублей" в метод _normalize.
+    При этом метод __init__ должен выглядеть таким образом:
+"""
+
+from pprint import pprint
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def _normalize(self, topology_dict):
+        return {
+            min(local, remote): max(local, remote)
+            for local, remote in topology_dict.items()
+        }
+
+if __name__ == "__main__":
+    topology_example = {
+        ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+        ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+        ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+        ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+        ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+        ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+        ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
+        ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
+        ("SW1", "Eth0/3"): ("R3", "Eth0/0"),}
+    top = Topology(topology_example)
+    pprint(top.topology)
